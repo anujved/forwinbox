c20644b74e8d581de4f590a4e43cd8136645ac5a
Anuj Gupta <vedanuj@yahoo.com>
2021-07-18 03:06:18 +0530

    with responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import './App.css';
 import { useSelector} from 'react-redux';
-
 import WinboxWrapper from "./Winbox"
 
 const Component2 = () =>{
--- src/Winbox.js
+++ src/Winbox.js
@@ -1,17 +1,48 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import "winbox";
 import ReactDOM from "react-dom";
 
-const WinboxWrapper = ({ title, id, children,isvisible,closeHelper }) => {
+const WinboxWrapper = ({
+  title,
+  id,
+  children,
+  isvisible,
+  closeHelper,
+  moveHelper,
+  focusHelper,
+  blurHelper,
+  resizeHelper
+}) => {
   useEffect(() => {
-      if(isvisible){
-          var div = document.createElement("div");
-          ReactDOM.render(<Provider store={store}>{children}</Provider>, div);
-          window.WinBox.new({ mount: div , onclose: function(force){ closeHelper && closeHelper(force) }});
-      }
+    if (isvisible) {
+      var div = document.createElement("div");
+      ReactDOM.render(<Provider store={store}>{children}</Provider>, div);
+      const winbox = window.WinBox.new({
+        id: "my-window",
+        class: ["no-full", "my-theme"],
+        root: document.body,
+        title: "All Options",
+        background: "#fff",
+        border: 4,
+        x: "center",
+        y: "center",
+        max: false,
+        splitscreen: true,
+        top: 50,
+        right: 50,
+        bottom: 0,
+        left: 50,
+        mount: div,
+        onfocus: focusHelper,
+        onblur: blurHelper,
+        onresize: resizeHelper,
+        onmove: moveHelper,
+        onclose: closeHelper,
+      });
+      winbox.resize("50%", "50%").move("center", "center");
+    }
   }, [title, id, children, isvisible]);
 
   return null;
